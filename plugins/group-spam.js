9673fa2a770ec959b1ef2908c364d4d8658bce86
AzeTegola <134860615+AzeTegola@users.noreply.github.com>
2023-05-29 19:17:51 +0200

    Update group-spam.js

--- plugins/group-spam.js
+++ plugins/group-spam.js
@@ -34,7 +34,7 @@ let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
     await m.reply(pesan)
     await m.reply(pesan)
 
-    conn.reply(m.chat, '📮 𝓔𝓢𝓢𝓔𝓝𝓣𝓘𝓐𝓛 𝓑𝓞𝓣 ✦ᵉᵈᶦᶻ ᴹᵒᵒⁿᶜˡᵒᵘᵈˢ', m)
+    conn.reply(m.chat, '𝑻𝑬𝑮𝑶𝑳𝑨-𝑩𝑶𝑻🍃', m)
 }
 handler.help = ['groupspam'].map(v => v + ' <teks>')
 handler.tags = ['group']
